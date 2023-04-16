3e3f42465d1fbfb201acf04004241316d02e6e57
REDDY KAMAL <81550994+singlereddy23@users.noreply.github.com>
2023-04-16 11:52:00 +0530

    Add files via upload

---
+++ JavaPart-1/src/com/pack01/Practical01.java
@@ -0,0 +1,12 @@
+package com.pack01;
+
+public class Practical01 {
+
+	public static void main(String[] args) {
+		System.out.println("1.Java is simple");
+		System.out.println("2.Java is simple");
+		System.out.println("3.Java is simple");
+
+	}
+
+}
